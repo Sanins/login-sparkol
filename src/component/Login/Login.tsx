f170b8182a96c53e52c47a4214082c482d78432c
Robert Sanins <rob.sanins@lovehoney.co.uk>
2020-02-13 14:22:51 +0000

    add axios package. Basic auth added to Login

--- src/component/Login/Login.tsx
+++ src/component/Login/Login.tsx
@@ -1,19 +1,59 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
 import './Login.scss';
+
+export function setAuthToken(token?:any) {
+	axios.defaults.headers.common['Authorization'] = '';
+	delete axios.defaults.headers.common['Authorization'];
+  
+	if (token) {
+	  axios.defaults.headers.common['Authorization'] = `${token}`;
+	}
+  }
 
 const Login: React.FC<{}> = () => {
 
 	// username: jeff1967
-	// password: lightbulb
+	// password: hotdog
 
 	const [form, setValues] = useState({
-        username: '',
-        password: '',
-    });
+        username: 'jeff1967',
+        password: 'hotdog',
+	});
+	
+	// const [isloading, setIsLoading] = useState(false);
+	const [isError, setIsError] = useState('');
+	const [loggedIn, setIsLoggedIn] =  useState(false);
 
 	const handleSubmit = (event:React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-		console.log(form.username, form.password);
+		event.preventDefault()
+
+		// setIsLoading(true);
+
+		axios({
+			method: 'POST',
+			url: 'http://localhost:3333/login',
+			data: {
+				username : form.username,
+				password : form.password
+			  }
+		})
+		.then( user => {
+			const token = user.data.token;
+			setAuthToken(token);
+			return axios.get('http://localhost:3333/verifyToken', {headers: { Authorization: `Bearer ${token}` }} )
+		})
+		.then((res) => {
+			console.log(res.data);
+			if (res.data === 'OK') {
+				setIsLoggedIn(true)
+				// setIsLoading(false);
+			}
+		})
+		.catch((response) => {
+			setIsError(response);
+			// setIsLoading(false);
+		});
 	};
 
 	const updateField = (e:React.FormEvent<HTMLInputElement>) => {
@@ -24,21 +64,39 @@ const Login: React.FC<{}> = () => {
             ...form,
             [name]: value
         });
-    };
+	};
+	
+	const logout = () => {
+		setAuthToken();
+		setIsLoggedIn(false);
+	}
 
 	return (
+		<>
+		{!loggedIn ?
 		<div className='login'>
 			<h2 className='login__heading'>Login</h2>
 			<form onSubmit={handleSubmit} >
 				<div>
-					<input className='login__input' placeholder='Email' id="username" name="username" type="text" onChange={updateField} value={form.username} />
+					<input className='login__input' placeholder='Username' id="username" name="username" type="text" onChange={updateField} value={form.username} />
 				</div>
 				<div>
 					<input className='login__input' placeholder='Password' id="password" name="password" type="text" onChange={updateField} value={form.password} />
 				</div>
 				<button className='login__button--submit' type="submit">Submit</button>
+				{isError && 
+					<p className='login__state--error'>Incorrect username or password</p> 
+				}
 			</form>
         </div>
+		:
+			<div>
+				<button className='login__button--submit' onClick={logout}>Log out</button>
+				<p>hello {form.username}</p>
+			</div>
+		}
+		
+		</>
 			
 	);
 };
